b3350fd7df88b380f42ccdf81fc24a8257b290ba
sefatanam <sefatanam@gmail.com>
2025-02-13 23:53:29 +0600

    working

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,24 +1,39 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local on_attach = require('nvchad.configs.lspconfig').on_attach
+local on_init = require('nvchad.configs.lspconfig').on_init
+
+
 local lspconfig = require "lspconfig"
-
+local util = require "lspconfig/util"
 -- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+-- local servers = { "html", "cssls", "ts_ls", "denols", "jsonls", "gopls", "rust_analyzer", "dockerls", "lua_ls" }
 
--- lsps with default config
+lspconfig.servers = { 'html', 'cssls', 'jsonls', 'lua_ls', 'dockerls', 'ts_ls', 'denols', 'rust_analyzer', 'gopls',
+  'angularls' }
+
+local servers = {
+  "html",
+  "cssls",
+  "jsonls",
+  "lua_ls",
+  "dockerls",
+  "ts_ls",
+}
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.angularls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  root_dir = util.root_pattern("angular.json", "angular.json", ".git", "nx.json", "project.json"),
+}
--- lua/custom/plugins/masonlsp.lua
+++ lua/custom/plugins/masonlsp.lua
@@ -30,6 +30,7 @@ return {
         "bashls",
         "lua_ls"
       },
+      auto_install = true,
     })
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -58,16 +59,37 @@ return {
           capabilities = capabilities,
         })
       end,
+      ["gopls"] = function()
+        lspconfig.gopls.setup({
+          on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+            require('nvchad.configs.lspconfig').on_attach(client, bufnr)
+          end,
+          on_init = require('nvchad.configs.lspconfig').on_init,
+          capabilities = capabilities,
+          cmd = { "gopls" },
+          filetypes = { "go", "gomod", "gotmpl", "gowork" },
+          root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+              },
+              completeUnimported = true,
+              usePlaceholders = true,
+              staticcheck = true,
+            },
+          },
+        })
+      end,
       ["angularls"] = function()
-        -- Load Mason's registry
         local ok, mason_registry = pcall(require, 'mason-registry')
         if not ok then
           vim.notify 'mason-registry could not be loaded'
           return
         end
-        -- Get Angular Language Server's install path from Mason
         local angularls_path = mason_registry.get_package('angular-language-server'):get_install_path()
-        -- Define the command to start ngserver with correct paths
         local cmd = {
           'ngserver',
           '--stdio',
@@ -82,16 +104,13 @@ return {
             vim.uv.cwd(),
           }, ','),
         }
-        -- LSP configuration for angularls
         local config = {
           cmd = cmd, -- The dynamically built command
           on_new_config = function(new_config, new_root_dir)
-            -- Update the command with new configuration if needed
             new_config.cmd = cmd
           end,
           root_dir = lspconfig.util.root_pattern('angular.json', '.git'),                           -- Detect Angular projects
           filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" }, -- File types for Angular
-          -- capabilities = require('nvchad.configs.lspconfig').capabilities,                          -- Capabilities, like autocompletion
           on_attach = require('nvchad.configs.lspconfig').on_attach,                                -- Attach custom LSP behaviors
           capabilities = capabilities,
         }
